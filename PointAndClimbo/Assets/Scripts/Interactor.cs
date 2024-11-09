750f6d324bb83f3783f9e83df48e7eae7a97f617
Boabb <ASTRUT300@caledonian.ac.uk>
2024-11-09 12:43:34 +0000

    Added "Interaction"

---
+++ PointAndClimbo/Assets/Scripts/Interactor.cs
@@ -0,0 +1,47 @@
+
+using System.Collections;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] float interactRange = 500;
+    bool interacting = false;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Interactable")
+        {
+            interacting = true;
+            StartCoroutine(Interact());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Interactable")
+        {
+            interacting = false;
+            StopCoroutine(Interact());
+        }
+    }
+
+    IEnumerator Interact()
+    {
+
+
+        while (interacting && Vector2.Distance(GameManager.player.transform.position, transform.position) <= interactRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Interacted!");
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+}
