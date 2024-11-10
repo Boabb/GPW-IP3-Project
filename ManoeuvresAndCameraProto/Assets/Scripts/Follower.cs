1404ed1eebfe77aa4a86c4733d397bfc75084b65
Boabb <ASTRUT300@caledonian.ac.uk>
2024-11-10 12:10:00 +0000

    Smooth Camera Following

--- ManoeuvresAndCameraProto/Assets/Scripts/Follower.cs
+++ ManoeuvresAndCameraProto/Assets/Scripts/Follower.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Follower : MonoBehaviour
 {
     [SerializeField] GameObject agentToFollow;
+    [SerializeField] float xOffset = 50;
+    [SerializeField] float cameraSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (agentToFollow != null)
+        {
+            agentToFollow = GameManager.player;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(agentToFollow.transform.position.x, agentToFollow.transform.position.y, transform.position.z);
+        if (agentToFollow != null)
+        {
+            Vector3 position = new(agentToFollow.transform.position.x, agentToFollow.transform.position.y, transform.position.z);
+            position.x -= xOffset;
+
+            transform.position = Vector3.Lerp(transform.position, position, cameraSpeed);
+        }
+        else
+        {
+            agentToFollow = GameManager.player;
+        }
     }
+    
 }
