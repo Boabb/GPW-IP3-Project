8a94373cb2e228754e31ea219f83eedd6d98f756
HagiwaraAkiraP <akira@park-hagiwara.net>
2024-11-19 11:50:29 +0000

    Began tidying code

---
+++ ManoeuvresAndCameraProto/Assets/Scripts/UserInput.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.ScrollRect;
+
+public class UserInput : MonoBehaviour
+{
+    void HoldLeft()
+    {
+        //moves you left by a speed
+        //changes animation
+        //changes sound effect
+    }
+
+    void HoldRight()
+    {
+        //moves you right by a speed
+        //changes animation
+        //changes sound effect
+    }
+
+    void TapLeft()
+    {
+        //begins climb manoeuvre 
+    }
+
+    void TapRight()
+    {
+        //begins climb manoeuvre
+    }
+
+    void TapJump() 
+    {
+        //jumps
+    }
+
+    void HoldInteract() 
+    {
+        //begins interaction with an interactable triggered by a hold
+    }
+
+    void TapInteract()
+    {
+        //begins interaction with an interactable triggered by a tap
+    }
+}
