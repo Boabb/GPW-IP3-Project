a4a5967ae06206709d47e53972d76e70a2ffc00c
Boabb <ASTRUT300@caledonian.ac.uk>
2025-02-26 14:26:00 +0000

    Update PlayerAnimator.cs

--- Project/Assets/Scripts/PlayerAnimator.cs
+++ Project/Assets/Scripts/PlayerAnimator.cs
@@ -33,11 +33,6 @@ public class PlayerAnimator : MonoBehaviour
         playerAnimator.speed = 0;
     }
 
-    public void PauseAnimation()
-    {
-        playerAnimator.speed = 0;
-    }
-
     public void PlayerIdle()
     {
         playerAnimator.speed = 1;
