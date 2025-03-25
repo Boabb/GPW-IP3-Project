4ef1e4104c5dabccf530fac383626592873e1b07
HagiwaraAkiraP <akira@park-hagiwara.net>
2025-03-25 07:56:33 +0000

    Resize images and text in context, fixed some simple issues to transition level 1

--- Project/Assets/Scripts/InteractableObjects/ZoomOut.cs
+++ Project/Assets/Scripts/InteractableObjects/ZoomOut.cs
@@ -14,7 +14,7 @@ public class ZoomOut : InteractableObject
 
     public override void Interaction(GameObject playerGO)
     {
-        playerGO.GetComponentInParent<PlayerData>().customPlayerVelocity = -50;
+        playerGO.GetComponentInParent<PlayerData>().customPlayerVelocity = -40;
         camCon.LerpToZoom(0.1f, 8.5f);
         camCon.LerpToPositionY(0.1f, 6.15f);
     }
