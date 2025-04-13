f1ce34138ecec8c2d040a788449433e003912f98
Boabb <ASTRUT300@caledonian.ac.uk>
2025-04-13 16:04:42 +0100

    Build added

--- Project/Assets/Scripts/AutoEvents/VentAutoEvent.cs
+++ Project/Assets/Scripts/AutoEvents/VentAutoEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//an auto event which changes the movement type of the player to crawl
+//an auto event which applies all the logic that should do so once the player enters the vent in Level 3 (Wallenberg House) changes the movement type of the player to crawl
 public class VentAutoEvent : AutoEvent
 {
     //NOT CURRENTLY FUNCTIONAL             
